50b2b2bcd1bab7e46eaf777318513b2cafb05253
Renan Bezerra de Lima <77756047+Renangenito@users.noreply.github.com>
2021-09-24 17:42:21 -0300

    melhorando o código e adicionando a página sucesso

--- validaFormulario.js
+++ validaFormulario.js
@@ -1,27 +1,28 @@
 const pegaNome = document.querySelector("#cadastro-nome")
-
 const pegaEmail = document.querySelector("#cadastro-email")
-
 const pegaTelefone = document.querySelector("#cadastro-telefone")
-
 const pegaMensagem = document.querySelector("#mensagem-sucesso")
+const botao = document.querySelector("#botao-enviar")
+const clintes = []
 
-function enviar(){
+botao.addEventListener("click", (event)=>{
+    
+    if(pegaNome.lenght > 0 && pegaEmail.lenght > 0 && pegaTelefone.lenght > 0){
+        const coletaDados = {
+            nome: pegaNome.value,
+            email: pegaEmail.value,
+            telefone: pegaTelefone.value
+            
+        }
+          console.log(coletaDados)
+         
+          limpaFormulario();
+    }else{
+        console.log("Erro, algum campo faltando preencher!")
+    }
  
-  const coletaDados = {
-      nome: pegaNome.value,
-      email: pegaEmail.value,
-      telefone: pegaTelefone.value
-  }
-    console.log(coletaDados)
-    mostraMensagem();
-    limpaFormulario();
-}
+})
 
-
-function mostraMensagem(){
-    pegaMensagem.textContent = "Parabéns, Cadastro realizado com sucesso!! \n \nVocê receberá um email de confirmação!"
-}
 
 function limpaFormulario(){
     pegaNome.value = "";
